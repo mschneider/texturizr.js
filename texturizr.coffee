5c4ea77fef94ae56626a61b5bb75a69fb76538d5
M.Schneider <not.m.schneider@gmail.com>
2012-01-09 19:33:43 +0100

    textures can now be combined

--- texturizr.coffee
+++ texturizr.coffee
@@ -1,31 +1,40 @@
 class Texture
-  constructor: (@bg, @size) ->
-    @canvas = document.createElement("canvas")
-    @canvas.width = @canvas.height = @size
-    ctx = @canvas.getContext "2d"
-    ctx.fillStyle = @bg
-    ctx.fillRect 0, 0, @size, @size
-    this.render()
 
-  applyTo: (element) ->
-    url = @canvas.toDataURL "image/png"
-    $(element).css "background", "url(" + url + ")"
-
-class StripedTexture extends Texture
-  render: () ->
+class ColorTexture extends Texture
+  constructor: (@fillStyle) ->
+  
+  render: (@canvas) ->
     ctx = @canvas.getContext "2d"
-    ctx.moveTo -1, -6
-    ctx.lineTo 11, 6
-    ctx.moveTo -6, -1
-    ctx.lineTo 6, 11
-    ctx.strokeStyle = "black"
-    ctx.stroke()
+    ctx.fillStyle = @fillStyle
+    ctx.fillRect 0, 0, @canvas.width, @canvas.height
+
+class StripeTexture extends Texture
+  constructor: (@strokeStyle, @offset, @lineWidth) ->
+
+  render: (@canvas) ->
+    @ctx = @canvas.getContext "2d"
+    x = y = @offset/2
+    while x < @canvas.width
+      this.drawStripe(x, 0)
+      x += @offset
+    while y < @canvas.height
+      this.drawStripe(0, y)
+      y += @offset
+    @ctx.strokeStyle = @strokeStyle
+    @ctx.lineWidth = @lineWidth
+    @ctx.stroke()
+
+  drawStripe: (x, y) ->
+    @ctx.moveTo x-@canvas.width, y-@canvas.height
+    @ctx.lineTo x+@canvas.width, y+@canvas.height
 
 class GrainTexture extends Texture
-  render: () ->
+  constructor: (@amount) ->
+
+  render: (@canvas) ->
     ctx = @canvas.getContext "2d"
     for x in [0..@canvas.width]
       for y in [0..@canvas.height]
-         opacity = Math.random() * 0.08
+         opacity = Math.random() * @amount
          ctx.fillStyle = "rgba(0, 0, 0, " + opacity + ")";
          ctx.fillRect(x, y, 1, 1);
--- texturizr.js
+++ texturizr.js
@@ -1,50 +1,68 @@
-var GrainTexture, StripedTexture, Texture;
+var ColorTexture, GrainTexture, StripeTexture, Texture;
 var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
 Texture = (function() {
 
-  function Texture(bg, size) {
-    var ctx;
-    this.bg = bg;
-    this.size = size;
-    this.canvas = document.createElement("canvas");
-    this.canvas.width = this.canvas.height = this.size;
-    ctx = this.canvas.getContext("2d");
-    ctx.fillStyle = this.bg;
-    ctx.fillRect(0, 0, this.size, this.size);
-    this.render();
-  }
-
-  Texture.prototype.applyTo = function(element) {
-    var url;
-    url = this.canvas.toDataURL("image/png");
-    return $(element).css("background", "url(" + url + ")");
-  };
+  function Texture() {}
 
   return Texture;
 
 })();
 
-StripedTexture = (function() {
+ColorTexture = (function() {
 
-  __extends(StripedTexture, Texture);
+  __extends(ColorTexture, Texture);
 
-  function StripedTexture() {
-    StripedTexture.__super__.constructor.apply(this, arguments);
+  function ColorTexture(fillStyle) {
+    this.fillStyle = fillStyle;
   }
 
-  StripedTexture.prototype.render = function() {
+  ColorTexture.prototype.render = function(canvas) {
     var ctx;
+    this.canvas = canvas;
     ctx = this.canvas.getContext("2d");
-    ctx.moveTo(-1, -6);
-    ctx.lineTo(11, 6);
-    ctx.moveTo(-6, -1);
-    ctx.lineTo(6, 11);
-    ctx.strokeStyle = "black";
-    return ctx.stroke();
+    ctx.fillStyle = this.fillStyle;
+    return ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
   };
 
-  return StripedTexture;
+  return ColorTexture;
+
+})();
+
+StripeTexture = (function() {
+
+  __extends(StripeTexture, Texture);
+
+  function StripeTexture(strokeStyle, offset, lineWidth) {
+    this.strokeStyle = strokeStyle;
+    this.offset = offset;
+    this.lineWidth = lineWidth;
+  }
+
+  StripeTexture.prototype.render = function(canvas) {
+    var x, y;
+    this.canvas = canvas;
+    this.ctx = this.canvas.getContext("2d");
+    x = y = this.offset / 2;
+    while (x < this.canvas.width) {
+      this.drawStripe(x, 0);
+      x += this.offset;
+    }
+    while (y < this.canvas.height) {
+      this.drawStripe(0, y);
+      y += this.offset;
+    }
+    this.ctx.strokeStyle = this.strokeStyle;
+    this.ctx.lineWidth = this.lineWidth;
+    return this.ctx.stroke();
+  };
+
+  StripeTexture.prototype.drawStripe = function(x, y) {
+    this.ctx.moveTo(x - this.canvas.width, y - this.canvas.height);
+    return this.ctx.lineTo(x + this.canvas.width, y + this.canvas.height);
+  };
+
+  return StripeTexture;
 
 })();
 
@@ -52,12 +70,13 @@ GrainTexture = (function() {
 
   __extends(GrainTexture, Texture);
 
-  function GrainTexture() {
-    GrainTexture.__super__.constructor.apply(this, arguments);
+  function GrainTexture(amount) {
+    this.amount = amount;
   }
 
-  GrainTexture.prototype.render = function() {
+  GrainTexture.prototype.render = function(canvas) {
     var ctx, opacity, x, y, _ref, _results;
+    this.canvas = canvas;
     ctx = this.canvas.getContext("2d");
     _results = [];
     for (x = 0, _ref = this.canvas.width; 0 <= _ref ? x <= _ref : x >= _ref; 0 <= _ref ? x++ : x--) {
@@ -65,7 +84,7 @@ GrainTexture = (function() {
         var _ref2, _results2;
         _results2 = [];
         for (y = 0, _ref2 = this.canvas.height; 0 <= _ref2 ? y <= _ref2 : y >= _ref2; 0 <= _ref2 ? y++ : y--) {
-          opacity = Math.random() * 0.08;
+          opacity = Math.random() * this.amount;
           ctx.fillStyle = "rgba(0, 0, 0, " + opacity + ")";
           _results2.push(ctx.fillRect(x, y, 1, 1));
         }
